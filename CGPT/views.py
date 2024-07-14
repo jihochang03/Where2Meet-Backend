07a6b45abc24985fa3553dd487d17c9c09d29e41
hoyah <hoyaho03@snu.ac.kr>
2024-07-14 13:42:31 +0900

    1번 팩터 해결 필요

--- CGPT/views.py
+++ CGPT/views.py
@@ -20,7 +20,8 @@ factor_keywords = {
     'factor_6': '액티비티',
     'factor_7': '쇼핑',
 }
-#chatgpt에 넣기
+
+# chatgpt에 넣기
 def get_completion(prompt):
     query = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -40,6 +41,10 @@ class QueryView(APIView):
 
     @swagger_auto_schema(
         operation_description="Get GPT-3.5 completion for a given station and selected factors",
+        manual_parameters=[
+            openapi.Parameter('station_name', openapi.IN_QUERY, type=openapi.TYPE_STRING, description='Name of the station'),
+            openapi.Parameter('factor', openapi.IN_QUERY, type=openapi.TYPE_STRING, description='Factors to consider', style='form', explode=True, enum=['factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6', 'factor_7'])
+        ],
         responses={
             status.HTTP_200_OK: openapi.Schema(
                 type=openapi.TYPE_OBJECT,
@@ -50,17 +55,17 @@ class QueryView(APIView):
             status.HTTP_400_BAD_REQUEST: 'Invalid query parameters',
         }
     )
-    #prompt 내용 넣기
     def get(self, request, format=None):
         station_name = request.query_params.get('station_name')
         factors = request.query_params.getlist('factor')
-
+        print(factors)
         if not station_name or not factors:
             return Response({'error': 'station_name and at least one factor are required'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         factor_keywords_list = [factor_keywords[factor] for factor in factors]
         factors_string = ', '.join(factor_keywords_list)
         prompt = f"약속장소로 '{station_name}'이 적합한 이유를 '{factors_string}' 관점에서 한줄로 설명해줘."
         print(prompt)
         response = get_completion(prompt)
         return Response({'response': response})
+
