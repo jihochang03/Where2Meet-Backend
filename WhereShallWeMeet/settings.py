5238257b6bb44de20b2dd3629ca09790e700d239
hoyah <hoyaho03@snu.ac.kr>
2024-07-16 15:44:46 +0900

    fly.io 전상태로 복구

--- WhereShallWeMeet/settings.py
+++ WhereShallWeMeet/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY =env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'whereshallwemeet.fly.dev']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : (
