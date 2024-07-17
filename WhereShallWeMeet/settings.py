41f8e11b6a958b0440b17116e4d1cd3d0e72c9ab
hoyah <hoyaho03@snu.ac.kr>
2024-07-18 01:25:45 +0900

    allowed host update

--- WhereShallWeMeet/settings.py
+++ WhereShallWeMeet/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY =env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['.amazonaws.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.ap-northeast-2.compute.amazonaws.com', '127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : (
