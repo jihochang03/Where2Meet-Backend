2a40625f8cca6483c92f11a4c4005e96e6dc76fd
hoyah <hoyaho03@snu.ac.kr>
2024-07-20 11:55:44 +0900

    피드백 수정 완료

--- FindBestStation/utils.py
+++ FindBestStation/utils.py
@@ -112,8 +112,8 @@ def find_nearest_stations_kakao(midpoint):
     }
     params = {
         "query": "지하철역",
-        "x": midpoint[1],  # 경도
-        "y": midpoint[0],  # 위도
+        "x": midpoint[0],  # 경도
+        "y": midpoint[1],  # 위도
         "radius": 20000,
         "sort": "distance",
         # "category_group_code": "SW8",
